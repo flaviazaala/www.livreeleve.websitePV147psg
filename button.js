8b9b8c9115d5f0d121cced6eb9ab446d529097d9
flaviazaala <101367494+flaviazaala@users.noreply.github.com>
2022-06-13 15:50:36 -0300

    Add files via upload

---
+++ button.js
@@ -0,0 +1,70 @@
+const actionCta = document.getElementById('cta');
+const actionCta2 = document.getElementById('cta2');
+
+actionCta.addEventListener('click', () => {
+  window.location.href = "https://pay.kiwify.com.br/tpSkFE5"
+});
+
+actionCta2.addEventListener('click', () => {
+  window.location.href = "https://pay.kiwify.com.br/tpSkFE5"
+});
+
+async function displayButton(){
+  // alterar somente o numero dos minutos
+  const minute = 26;
+  const timeInMiliseconds = await convertMinutesToMiliseconds(minute);
+
+  setTimeout(() => {
+    actionCta.style.display = "flex";
+    actionCta2.style.display = "flex";
+  }, timeInMiliseconds);
+}
+
+async function convertMinutesToMiliseconds(minutes){
+  const seconds = minutes * 60;
+
+  const miliseconds = seconds * 1000;
+
+  return miliseconds;
+}
+
+displayButton();
+
+// right click
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// f12
+document.onkeypress = function (event) {
+  event = (event || window.event);
+  if (event.keyCode == 123) {
+    return false;
+  }
+}
+document.onmousedown = function (event) {
+  event = (event || window.event);
+  if (event.keyCode == 123) {
+    return false;
+  }
+}
+document.onkeydown = function (event) {
+  event = (event || window.event);
+  if (event.keyCode == 123) {
+    return false;
+  }
+}
+
+document.onkeydown = function (event) {
+  var pressedKey = String.fromCharCode(event.keyCode).toLowerCase();
+
+  if (event.ctrlKey && (pressedKey == "c" || pressedKey == "u")) {
+    return false;
+  }
+}
+
+document.onkeydown = function (event) {
+  if (event.keyCode == 123) { // Prevent F12
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+    return false;
+  }
+}
